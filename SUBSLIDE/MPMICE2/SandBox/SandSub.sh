c96c932b3e94cd57d880c612790b892730a74e3a
Quoc Anh Tran <tranquocanh90@gmail.com>
2022-03-23 16:45:42 +0100

    add SandBox example

---
+++ SUBSLIDE/MPMICE2/SandBox/SandSub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=mpmice2
+#SBATCH --account=nn9827k
+#SBATCH --time=4-00:00:00
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=128
+
+## Set up job environment:
+set -o errexit  # Exit the script on any error
+set -o nounset  # Treat any unset variables as an error
+
+## Do some work:
+cd /cluster/projects/nn9827k/Simulation2D/SandBox/
+
+module load OpenMPI/4.0.3-GCC-9.3.0
+
+mpirun -np 1024 /cluster/projects/nn9827k/Uintah/optQA/StandAlone/sus SandSub.ups
+
+tar czf SandSub.tar.gz SandSub.uda.000
+
+exit 0
