e496467d0fe454d1f01d638650638574f7e85d7f
Quoc Anh Tran <tranquocanh90@gmail.com>
2022-08-12 07:24:49 +0200

    clean Mohr Coulomb

--- src/CCA/Components/MPM/Materials/ConstitutiveModel/SoilModels/MohrCoulomb.cc
+++ src/CCA/Components/MPM/Materials/ConstitutiveModel/SoilModels/MohrCoulomb.cc
@@ -401,39 +401,7 @@ void MohrCoulomb::initializeCMData(const Patch* patch,
             if (flag->d_initial_stress == "Gjerdrum3D") {
                 if (i == 2) {
 
-                    //if (pColor[*iter] > 50){
                     ISVs[2][*iter] = pColor[*iter]*1000;
-                    //}
-                    //else { ISVs[2][*iter] = 50000; }
-
-                    //double z = px[*iter](2);
-                    //if (z < 150) ISVs[2][*iter] = 68000 + 3000 * (150 - z);
-                    
-                    /*
-                    if (flag->d_insertUndrainedShearStrengthFile != "") {
-                        std::ifstream is(flag->d_insertUndrainedShearStrengthFile.c_str());
-                        if (!is) {
-                            throw ProblemSetupException("ERROR Opening undrained shear strength file '" + flag->d_insertUndrainedShearStrengthFile + "'\n",
-                                __FILE__, __LINE__);
-                        }
-                        double x = px[*iter](0);
-                        double y = px[*iter](1);
-                        double z = px[*iter](2);
-                            while (is) {
-                                double x_file, y_file, z_file, Su_file;
-                                is >> x_file >> y_file >> z_file >> Su_file;
-
-                                if (is) {
-                                    if (x_file - x <= 0.01 && y_file - y <= 0.01 && z_file - z <= 0.01) {
-
-                                        cerr << Su_file << endl;
-                                        ISVs[2][*iter] = Su_file;
-                                        cerr << ISVs[2][*iter] << endl;
-                                        continue;
-                                    }
-                                }
-                            }
-                    }*/
                 }
             }
 
