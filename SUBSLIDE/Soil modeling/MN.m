17bb594470c1b18b2659710ca407dc12622040d4
Quoc Anh Tran <tranquocanh90@gmail.com>
2023-02-25 08:55:27 +0100

    update Matlab code for Matsuoka Nakai

--- SUBSLIDE/Soil modeling/MN.m
+++ SUBSLIDE/Soil modeling/MN.m
@@ -1,15 +1,15 @@
 % Simple DP Mob
 function [] = MN()
-E = 10000;                                      % Young's modulus
+E = 1000000;                                      % Young's modulus
 nuy = 0.3;                                      % Poisson's ratio
 lambda_c = 1;                                  % Dilatancy parameters
 M = 2 * sqrt(2) * tan (30/180*pi());            % Friction parameters
 % Epsilon_v_cs = 0.05;                             % Critical state volume strain
 % N0 = M*(-1+exp(-lambda_c*(Epsilon_v_cs-0)));     % Dilatancy state variables
-N0 = -0.2;
+N0 = 0.0;
 
 % Intial stress
-Sigma = [150 0 0; 0 150 0;0 0 150];        
+Sigma = [0 0 0; 0 0 0;0 0 0];        
 % 3 x 3 Stress tensor
 Sigma1(1,1) = 150; Sigma1(1,2) = 0;
 % Elastic matrix
@@ -22,6 +22,20 @@ D = E/(1+nuy)/(1-2*nuy)*[ 1-nuy nuy nuy 0 0 0 ...
 % Strain controled
 Epsilon(:,1) = zeros(6,1);
 % deps = [0.0001; -0.00005; -0.00005; 0; 0; 0];
+% deps = [-6.44226e-21; 3.72267e-20; -1.35091e-22; -3.48949e-20; 0; 0];
+% sig0 = [2.43018e-15; 5.67042e-15; 2.43018e-15; 4.71942e-16; 0; 0];
+
+% deps = [-0.00343274; -0.00440305; -0.000562585; 0.000394117; 0; 0];
+% deps=deps/10;
+% sig0 = [8619.95; 11393.9; 6304.87; 418.304; 0; 0];
+
+% deps = [-8.73686e-05; 0.000292047;-8.73686e-05; -6.50489e-05; 0; 0];
+% sig0 = [65.9657; 154.008; 65.9657; -5.11951; 0; 0];
+
+deps = [0; 4.84595e-28;0; 0; 0; 0];
+sig0 = [0; 0; 0; 0; 0; 0];
+
+
 N=N0;
 Nk(1,1) = N0;
 
@@ -30,20 +44,20 @@ for k = 2:5000
 % Plastic multiplier 
 lambda = 0;    
 N = N0;
+depsp=zeros(6,1);
 % Elastic trial
-sig0 =  [Sigma(1,1); Sigma(2,2); Sigma(3,3); Sigma(1,2); Sigma(1,3); Sigma(2,3)];
 
-if k < 4000
-    deps = [0.0001; -0.00005; -0.00005; 0; 0; 0];
-else
-    deps = [0.0001; 0; -0; 0; 0; 0];
-end
-
-sig = sig0 + D * deps;        % Stress vector
+% if k < 4000
+%     deps = [0.0001; -0.00005; -0.00005; 0; 0; 0];
+% else
+%     deps = [0.0001; 0; -0; 0; 0; 0];
+% end
+sigInc = D * deps;
+sig = sig0 + sigInc;        % Stress vector
 
 Sigma = [sig(1) sig(4) sig(5); sig(4) sig(2) sig(6);sig(5) sig(6) sig(3)];     
 I1 = trace(Sigma);
-I2 = 0.5*(trace(Sigma)^2 - trace(Sigma^2));
+I2 = 0.5*(I1^2 - trace(Sigma^2));
 I3 = det (Sigma);
 X = sqrt (I1*I2/I3 - 9);
 y = X - (M+N);                % Yield function
@@ -68,7 +82,7 @@ if y > 0
             Sigma_Inverse_Dev = Sigma_Inverse - 1/3*eye(3,3)*trace(Sigma_Inverse);
             Sigma_Inverse_Dev_Vector = [Sigma_Inverse_Dev(1,1); Sigma_Inverse_Dev(2,2); Sigma_Inverse_Dev(3,3); Sigma_Inverse_Dev(1,2); Sigma_Inverse_Dev(1,3); Sigma_Inverse_Dev(2,3)];
             N_vector = [N/3;N/3;N/3;0;0;0];
-            B = N_vector + I1 / X * Sigma_Inverse_Dev_Vector;
+            B = -(N_vector + I1 / X * Sigma_Inverse_Dev_Vector);
             
             % A
             A = lambda_c * (M + N) * N;
@@ -91,13 +105,14 @@ if y > 0
             % Vector
             dYdSigma = [dYdSigma(1,1) dYdSigma(2,2) dYdSigma(3,3) dYdSigma(1,2) dYdSigma(1,3) dYdSigma(2,3)];
             
-            dlambda = y / (A - dYdSigma * D * B);
-%             dlambda = y / (-dYdSigma * D * B);
+            dYdSigmaD = dYdSigma * D;
+            C = dYdSigma * D * B;
             
-            lambda = lambda + dlambda;
+            dlambda = y / (A + C)
+%             lambda = lambda + dlambda;
             
             % Flow rule           
-            depsp = - lambda * B;
+            depsp = depsp + dlambda * B;
             
             % Update stress
             depse = deps - depsp;
@@ -106,34 +121,19 @@ if y > 0
             % Update state variables
             Epsilon_v_plastic = depsp(1) + depsp(2) + depsp(3);
             N =  N0 + lambda_c*(M+N)*Epsilon_v_plastic;
-            
-            % Liquefaction cut-off on p
-%             p = sig(1)+sig(2)+sig(3);
-            
-%             if sig(1) <= 10 
-%                 sig(1:6) = 0;
-%             end
-%             
-%             if sig(2) <= 10 
-%                 sig(1:6) = 0;
-%             end
-%             
-%             if sig(3) <= 10 
-%                 sig(1:6) = 0;
-%             end
-            
+                       
             % Recompute yield function
             % Stress invariants
             Sigma = [sig(1) sig(4) sig(5); sig(4) sig(2) sig(6);sig(5) sig(6) sig(3)];     
             I1 = trace(Sigma);
-            I2 = 0.5*(trace(Sigma)^2 - trace(Sigma^2));
+            I2 = 0.5*(I1^2 - trace(Sigma^2));
             I3 = det (Sigma);
             
-            X = sqrt (I1*I2/I3 - 9);
+            X = sqrt (I1*I2/I3 - 9)
             
-            y = X - (M+N);
+            y = X - (M+N)
             
-            if I3 == 0
+            if I3 <= 0
                 y = 0;
                 sig(1:6) = 0;
             end
